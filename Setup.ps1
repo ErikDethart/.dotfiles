96bdd929d8cd747941a640816518cc5dd67e46c9
Erik Dethart <mail@erikdethart.com>
2023-09-07 15:49:20 -0500

    Include angular setup

--- Setup.ps1
+++ Setup.ps1
@@ -3,6 +3,10 @@ Write-Output "Starting Setup"
 Write-Output "Installing Packages"
 winget import windows-packages
 
+Write-Output "Setting up Angular"
+npm install -g @angular/cli
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
 //Finish
 Write-Output "Finished"
 pause
